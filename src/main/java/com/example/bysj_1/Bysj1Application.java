09fabfed402ba753fe00e29fcd788f5e2624f478
gaojian <ss2200hz@163.com>
2019-01-02 14:59:07 +0800

    1

---
+++ src/main/java/com/example/bysj_1/Bysj1Application.java
@@ -0,0 +1,14 @@
+package com.example.bysj_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bysj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bysj1Application.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/bysj_1/Bysj1ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.bysj_1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Bysj1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
